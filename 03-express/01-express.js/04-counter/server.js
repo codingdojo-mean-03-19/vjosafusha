e930727f5e2b6bb32567aa1ce82f522f1a8d35d4
Vjosa Fusha <vjosa.fusha@gmail.com>
2019-04-01 20:06:38 +0200

    counter

---
+++ 03-express/01-express.js/04-counter/server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var session = require('express-session');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(
+  session({
+    secret: 'abcdefghijklmnop',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+
+app.get('/', function(req, res) {
+  if (!req.session.counter) {
+    req.session.counter = 1;
+  } else {
+    req.session.counter++;
+  }
+
+  res.render('index', { counter: req.session.counter });
+});
+
+app.get('/plustwo', function(req, res) {
+  req.session.counter = req.session.counter + 1;
+  res.redirect('/');
+});
+
+app.get('/reset', function(req, res) {
+  req.session.counter = null;
+
+  res.redirect('/');
+});
+
+app.listen(8000, function() {
+  console.log('listening on port 8000');
+});
