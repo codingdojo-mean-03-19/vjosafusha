3a8706bb058680a284642caaf6b9d768ee8ac09c
Vjosa Fusha <vjosa.fusha@gmail.com>
2019-04-08 11:44:33 +0200

    04-group chat

--- 03-express/02-socket.io/04-group-chat/server.js
+++ 03-express/02-socket.io/04-group-chat/server.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const app = express();
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const server = app.listen(8000);
-const io = require('socket.io')(server);
-
-const messages = [];
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(path.join(__dirname, './static')));
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-app.use(
-  session({
-    secret: 'a1s2d3f4',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 },
-  })
-);
+var server = app.listen(8000, function() {});
+var io = require('socket.io').listen(server);
+var id = 0;
+var messages = {};
+var users = {};
+io.sockets.on('connection', function(socket) {
+  socket.on('got_new_user', function(data) {
+    users[socket.id] = { name: data.name };
+    socket.emit('existing_messages', messages);
+    io.emit('display_new_user', { name: data.name });
+  });
+  socket.on('new_message', function(data) {
+    messages[id] = { name: data.name, message: data.message };
+    io.emit('update_messages', messages[id]);
+    id++;
+  });
+  socket.on('disconnect', function() {
+    io.emit('user_disconnect', users[socket.id]);
+  });
+});
 
 app.get('/', function(req, res) {
   res.render('index');
-});
-
-io.sockets.on('connection', function(socket) {
-  socket.on('posting_message', function(data) {
-    messages.push({ name: data.name, message: data.content });
-    io.emit('new_message', { name: data.name, message: data.content });
-  });
 });
