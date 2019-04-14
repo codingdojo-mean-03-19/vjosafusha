9b582667510399c6a30b94158ad404bd8089bda7
Vjosa Fusha <vjosa.fusha@gmail.com>
2019-04-14 17:29:06 +0200

    modularized the controllers

--- 04-mongodb/04-modularization/01-04-modular-quoting-dojo/server/config/routes.js
+++ 04-mongodb/04-modularization/01-04-modular-quoting-dojo/server/config/routes.js
@@ -1,35 +1,15 @@
-var mongoose = require("mongoose");
-var User = mongoose.model('User');
+var quotes = require('../controllers/quotes.js');
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.render('index');
+        quotes.index(req, res);
     });
 
-    app.post('/quotes', function (req, res) {
-        console.log('POST DATA', req.body);
-        var user = new User({ name: req.body.name, quote: req.body.quote });
-        user.save(function (err) {
-            if (err) {
-                console.log('We have an error!', err);
-                for (var key in err.errors) {
-                    JSON.stringify(req.flash('submitting', err.errors[key].message));
-                }
-                res.redirect('/');
-            } else {
-                console.log('successfully submitted');
-                res.redirect('/quotes');
-            }
-        });
+    app.post('/quotes', function(req, res){
+        quotes.create(req, res);
     });
 
-    app.get('/quotes', function (req, res) {
-        User.find({}, function (err, users) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render('quotes', { msg: 'Users and Quotes', users });
-            }
-        }).sort({ createdAt: 'desc' });
-    });
+    app.get('/quotes', function(req, res){
+        quotes.display(req, res);
+    } );
 }
---
+++ 04-mongodb/04-modularization/01-04-modular-quoting-dojo/server/controllers/quotes.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose')
+var User = mongoose.model('User');
+
+module.exports = {
+    index: function (req, res) {
+        res.render('index');
+    },
+
+    create: function (req, res) {
+        console.log('POST DATA', req.body);
+        var user = new User({ name: req.body.name, quote: req.body.quote });
+        user.save(function (err) {
+            if (err) {
+                console.log('We have an error!', err);
+                for (var key in err.errors) {
+                    JSON.stringify(req.flash('submitting', err.errors[key].message));
+                }
+                res.redirect('/');
+            } else {
+                console.log('successfully submitted');
+                res.redirect('/quotes');
+            }
+        });
+    },
+
+    display: function (req, res) {
+        User.find({}, function (err, users) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('quotes', { msg: 'Users and Quotes', users });
+            }
+        }).sort({ createdAt: 'desc' });
+    }
+
+}
