8c962813195ec4162a3a3823dc00acb045b5225e
Vjosa Fusha <vjosa.fusha@gmail.com>
2019-04-16 22:13:37 +0200

    Hello Angular done

---
+++ 05-angular/00-platform/02-restful-task-api/server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    port = 8000;
+
+app.use(bodyParser.json());
+app.use(express.static( __dirname + '/angular-app/dist/angular-app' ));
+
+var routes = require('./server/config/routes')(app);
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
+
+
