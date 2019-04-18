604fee3572c3c3d41e4794ced75ddb7885a76ec1
Vjosa Fusha <vjosa.fusha@gmail.com>
2019-04-18 13:19:27 +0200

    ts bikes-oop

---
+++ 05-angular/01-overview-and-integration/02-typescript-bikes-oop.ts
@@ -0,0 +1,47 @@
+class Bike {
+    price: number;
+    max_speed: number;
+    miles: number;
+
+    constructor(prc: number, max_s: number) {
+        this.price = prc;
+        this.max_speed = max_s;
+        this.miles = 0;
+    }
+
+    // displayInfo() - have this method display the bike's price, maximum speed, and the total miles.
+    displayInfo() {
+        console.log(`Bike price: ${this.price} \n\tMaximum Speed: ${this.max_speed} \n\tTotal miles: ${this.miles}`)
+    }
+
+    // ride() - have it display "Riding" on the screen and increase the total miles ridden by 10
+    ride() {
+        console.log('riding +10 miles');
+        this.miles += 10;
+        return this;
+    }
+
+    // reverse() - have it display "Reversing" on the screen and decrease the total miles ridden by 5...
+    reverse() {
+        console.log('reversing -5 miles');
+        if (this.miles >= 5)
+            this.miles -= 5;
+        return this;
+    }
+
+
+}
+
+
+var bike = new Bike(1000, 240);
+var bike2 = new Bike(3000, 300);
+var bike3 = new Bike(1500, 200);
+
+// Have the first instance ride three times, reverse once and have it displayInfo(). 
+bike.ride().ride().ride().reverse().displayInfo();
+
+// Have the second instance ride twice, reverse twice and have it displayInfo().
+bike2.ride().ride().reverse().reverse().displayInfo();
+
+// Have the third instance reverse three times and displayInfo().
+bike3.reverse().reverse().reverse().displayInfo();
