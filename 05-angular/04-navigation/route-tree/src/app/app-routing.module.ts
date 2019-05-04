c33f7fccaefeec55a7693f00b3fdf6a12098b2fc
Vjosa Fusha <vjosa.fusha@gmail.com>
2019-05-04 16:30:13 +0200

    route tree done

--- 05-angular/04-navigation/route-tree/src/app/app-routing.module.ts
+++ 05-angular/04-navigation/route-tree/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
-import { AppComponent } from './app.component';
 import { ProductDetailsComponent } from './products/product-details/product-details.component';
 
 const routes: Routes = [
